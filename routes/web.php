964048346161e169cd4a8acb30da4fbdd639e528
tata <daraputrinatasukma@gmail.com>
2026-03-06 14:26:48 +0700

    chore: resolve conflict routes web

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/perkenalan', function () {
+    return '<h1>Halo! Nama saya Dara Putri N.S</h1>
+            <p>NIM: 4124016 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
+});
+
 Route::get('/perkenalan-nelamaulana', function () {
     return '<h1>Halo! Saya Nelamaulana</h1>
             <p>NIM: (isi NIM teman) | Prodi: Sistem Informasi</p>
